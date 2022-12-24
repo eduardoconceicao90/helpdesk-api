825569b4f816a9e15f5650aa877a1d7500b76957
Eduardo Conceição <eduardosaconceicao@gmail.com>
2022-12-24 12:43:32 -0300

    Classes de domínio criadas

---
+++ src/main/java/com/helpdeskapi/domain/Tecnico.java
@@ -0,0 +1,37 @@
+package com.helpdeskapi.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.helpdeskapi.domain.enums.Perfil;
+
+@Entity
+public class Tecnico extends Pessoa {
+
+	private static final long serialVersionUID = 1L;
+
+	@OneToMany(mappedBy = "tecnico")
+	private List<Chamado> chamados = new ArrayList<>();
+
+	public Tecnico() {
+		super();
+		addPerfil(Perfil.TECNICO);
+	}
+
+	public Tecnico(Long id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.TECNICO);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+
+}
