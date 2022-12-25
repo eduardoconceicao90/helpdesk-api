69a144d324f0da0b7f567f57d1c08008e5961295
Eduardo Conceição <eduardosaconceicao@gmail.com>
2022-12-25 16:14:44 -0300

    Validação nos campos de criação de novo usuário

--- src/main/java/com/helpdeskapi/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/helpdeskapi/resource/exception/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,7 +18,7 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e, HttpServletRequest request){
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), 
-				"Object Not Found", e.getMessage(), request.getRequestURI());
+				"Object Not Found Exception", e.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
@@ -24,8 +26,20 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(DataIntegratyViolationException.class)
 	public ResponseEntity<StandardError> dataIntegratyViolationException(DataIntegratyViolationException e, HttpServletRequest request){
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), 
-				"Violação de dados", e.getMessage(), request.getRequestURI());
+				"Data Integraty Violation Exception", e.getMessage(), request.getRequestURI());
 		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException e, HttpServletRequest request) {
+		ValidationError error = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), 
+				"Validation Errors", "Erro na validação dos campos", request.getRequestURI());
+
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			error.addErrors(x.getField(), x.getDefaultMessage());
+		}	
+
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
